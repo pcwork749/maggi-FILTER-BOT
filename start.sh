f37404e7e000f820d83f0603a2dc95da2a73d78c
pcwork749 <148769407+pcwork749@users.noreply.github.com>
2024-01-13 20:59:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,12 +5,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/pcwork749/maggi-FILTER-BOT.git /maggi-FILTER-BOT
+  git clone https://github.com/VJBots/VJ-FILTER-BOT.git /VJ-FILTER-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /maggi-FILTER-BOT 
+  git clone $UPSTREAM_REPO /VJ-FILTER-BOT 
 fi
-cd /maggi-FILTER-BOT 
+cd /VJ-FILTER-BOT 
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
